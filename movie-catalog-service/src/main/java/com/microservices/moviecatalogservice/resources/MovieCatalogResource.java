98b45393c09b9a05131f422146db4bd6adbd0692
rkarthikraj <rkarthykraj@gmail.com>
2022-03-30 23:30:46 +0530

    Using WebClient to make API calls

--- movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/microservices/moviecatalogservice/resources/MovieCatalogResource.java
@@ -4,16 +4,14 @@ import com.microservices.moviecatalogservice.models.CatalogItem;
 import com.microservices.moviecatalogservice.models.Movie;
 import com.microservices.moviecatalogservice.models.Rating;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,17 +22,34 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         List<Rating> ratings = Arrays.asList(
                 new Rating("1", 4),
                 new Rating("2", 5));
 
-        return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-            return new CatalogItem(movie.getMovieName(), movie.getMovieDescription(), rating.getMovieRating());
-        }).collect(Collectors.toList());
+        /*
+         RestTemplate:
+         return ratings.stream().map(rating -> {
+         Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+         return new CatalogItem(movie.getMovieName(), movie.getMovieDescription(), rating.getMovieRating());
+         }).collect(Collectors.toList());
+         */
 
         // return Collections.singletonList(new CatalogItem("Doctor Strange", "Thriller, Scientific", 5));
+
+        // WebClient
+        return ratings.stream().map(rating -> {
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+            return new CatalogItem(movie.getMovieName(), movie.getMovieDescription(), rating.getMovieRating());
+        }).collect(Collectors.toList());
     }
 }
